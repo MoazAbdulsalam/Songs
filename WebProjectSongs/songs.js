64df52955ab7914f77a835277da89b756adfb7f4
Moaz Abdulsalam <dod998849@gmail.com>
2025-10-17 04:28:03 +0300

    Add files via upload

---
+++ WebProjectSongs/songs.js
@@ -0,0 +1,566 @@
+const songs =
+{
+    CAS:
+    {
+        "Flash":
+        {
+            url:"https://www.youtube.com/embed/nrWC_z6l13w",
+            lyrics :`Flash:
+                    
+  Cigarettes After Sex
+  ----------------------
+  I'm a flash
+  You were blinded by the love I had
+
+  I'm a flash
+  The light could only get in through the cracks
+
+  *******************************************
+  *  And you've gotta do the right thing    *  
+  *  Do the right thing baby                * 
+  *  You're the white swan in my photograph *
+  *  Do the right thing                     *
+  *  Do the right thing baby                *
+  *  You're the white swan in my photograph *
+  *******************************************
+
+  When they crash the helicopters in my heart it ripped
+  Cut in half, you saw me lying there bleeding to death
+
+  *******************************************
+  *  And you've gotta do the right thing    *  
+  *  Do the right thing baby                * 
+  *  You're the white swan in my photograph *
+  *  Do the right thing                     *
+  *  Do the right thing baby                *
+  *  You're the white swan in my photograph *
+  *******************************************
+
+  Now I stand on the stage
+  I forget my lines to your play, "Satan's Imagination"
+  And a voice in my head's causing suicide searching for any way
+  To remember them
+
+  *******************************************
+  *  And you've gotta do the right thing    *  
+  *  Do the right thing baby                * 
+  *  You're the white swan in my photograph *
+  *  Do the right thing                     *
+  *  Do the right thing baby                *
+  *  You're the white swan in my photograph *
+  *******************************************`
+        },
+        "Apocalypes":
+        {
+            url:"https://www.youtube.com/embed/sElE_BfQ67s",
+            lyrics:`Apocalypse:
+
+  Cigarettes After Sex
+  --------------------
+  You leapt from crumbling bridges, watching cityscapes turn to dust
+  Filming helicopters crashing in the ocean from way above
+
+  Got the music in you, baby, tell me why
+  Got the music in you, baby, tell me why
+  You've been locked in here forever, and you just can't say goodbye
+
+  Kisses on the foreheads of the lovers wrapped in your arms
+  You've been hiding them in hollowed-out pianos left in the dark
+
+  Got the music in you, baby, tell me why
+  Got the music in you, baby, tell me why
+  You've been locked in here forever, and you just can't say goodbye
+
+  Your lips, my lips
+  Apocalypse
+  Your lips, my lips
+  Apocalypse
+
+  Go and sneak us through the rivers
+  Flood is rising up on your knees
+  Oh, please
+
+  Come out and haunt me, I know you want me
+  Come out and haunt me
+
+  Sharing all your secrets with each other, since you were kids
+  Sleeping soundly with the locket that she gave you clutched in your fist
+
+  Got the music in you, baby, tell me why
+  Got the music in you, baby, tell me why
+  You've been locked in here forever, and you just can't say goodbye
+  You've been locked in here forever, and you just can't say goodbye
+
+  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  ^ Oh, when you're all alone ^
+  ^ I will reach for you      ^
+  ^ When you're feeling low   ^
+  ^ I will be there too       ^
+  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ `   
+        },
+        "Neon Moon":
+        {
+            url:"https://www.youtube.com/embed/BrzXW140-HM",
+            lyrics:`Neon Moon:
+                    
+  Cigarettes After Sex
+  ---------------------
+  When the sun goes down on my side of town
+  That lonesome feeling comes to my door and the whole world turns blue
+
+  There's a rundown bar across the railroad tracks
+  Got a table for two way in the back
+  Where I sit alone and think of you
+
+  I spend most every night beneath the light of a neon moon
+
+  If you lose your one and only
+  There's always room here for the lonely
+  To watch your broken dreams
+  Dance in and out of the beams of a neon moon
+
+  I think of two young lovers running wild and free
+  Close my eyes and sometimes see you
+  In the shadows of this smoke-filled room
+
+  No telling how many tears I've sat here and cried
+  Or how many lies that I've lied
+  Telling my poor heart you'll come back one day...
+
+  I spend most every night beneath the light of a neon moon
+
+  If you lose your one and only
+  There's always room here for the lonely
+  To watch your broken dreams
+  Dance in and out of the beams of a neon moon
+
+  But I'll be alright as long as there's light from a neon moon
+  But I'll be alright as long as there's light from a neon moon...`
+        },
+        "Touch":
+        {
+            url:"https://www.youtube.com/embed/UwTqcctD1rw",
+            lyrics:`Touch:
+  
+  Cigarettes After Sex
+  ---------------------
+  
+  Oh, I missed you and I cried
+  But I said that I was alright
+  And I know it's been a while
+  Since I needed a distraction
+
+  'Cause I wanna do everything
+  That you want me to, to tell you the truth
+  I need to stay alive
+  So sad that I could die
+
+  It's leading me on, every time we touch
+  Leading me on, every time it hurts
+  Feeling so lonely, 'cause it's not enough
+  Missin' you only ever since we fell in love
+
+  And I watched you do a line
+  That you spread out on the table
+  I don't need to see you wired
+  I just wanna see you upside down
+
+  I wanna do everything
+  That you want me to, to tell you the truth
+  I listen to you cry all night
+  Through the telephone
+
+  It's leading me on, every time we touch
+  Leading me on, every time it hurts
+  Feeling so lonely, 'cause it's not enough
+  Missin' you only ever since we fell in love
+
+  Couldn't say you didn't want me there
+  When we were lyin' in my bed that night
+  You were acting like a different person
+  As if, suddenly, you wanted to just be friends
+  I was hurting so I took a shower
+  So you couldn't hear me, knowing I had to cry
+  Sad to know that while my heart was breaking
+  You were sleeping or just faking
+
+  It's leading me on, every time we touch
+  Leading me on, every time it hurts
+  Feeling so lonely, 'cause it's not enough
+  Missin' you only ever since we fell in love`
+        },
+        "Opera House":
+        {
+            url:"https://www.youtube.com/embed/giEOcBLcnfE",
+            lyrics:`Opera House:
+
+  Cigarettes After Sex
+  ---------------------
+  Built an opera house for you in the deepest jungle
+  And I walked across its stage, singing with my eyes closed
+  I've got a love for you I just can't escape
+  All of my love for you cuts me like barbed wire
+
+  Ooh, I was meant to love you
+  And always keep you in my life
+  Ooh, I was meant to love you
+  I knew I loved you at first sight
+
+  If I abandoned love, I'd be a man without dreams (ooh)
+  I'd rather be out there staring death right between its eyes now
+  And I can still hear the sound of you crying through the night
+  There in the opera house with no one else for miles
+
+  Ooh, I was meant to love you
+  And always keep you in my life
+  Ooh, I was meant to love you
+  I knew I loved you at first sight
+
+  Ooh, I was meant to love you
+  And always keep you in my life
+  Ooh, I was meant to love you
+  I knew I loved you at first sight
+                    `
+        },
+        "Bubblegum":
+        {
+            url:"https://www.youtube.com/embed/Y4lU3EdvnUU",
+            lyrics:`Bubblegum:
+
+  Cigarettes After Sex
+  ---------------------
+  Licking the barrel of a gun
+  Chewing it like it's bubblegum
+  Wave it like a magic wand
+  Pop a few rounds out just for fun
+  
+  Go to the strip club all alone
+  Thinking that I could take you home
+  To picture you with those other guys
+  It's driving me crazy all the time
+  
+  Well, I never wanted to be with you
+  But I think I'm falling, so what can I do?
+  You'll just lead me on 'til I'm drowning in tears
+  
+  Oh, and you're holding me
+  Oh, and you're holding me
+  Oh, and you're holding me
+  As if I was the last man standing on earth
+  
+  Well, I never wanted to be with you
+  But I think I'm falling, so what can I do?
+  You'll just lead me on 'til I'm drowning in tears `
+        }
+
+
+
+    },
+    GhostlyKisses:
+    {
+        "Stay":
+        {
+            url:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A742981408&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+            lyrics:`Stay:
+           
+     Ghostly Kisses
+     --------------
+     You left without no goodbye in the night
+     Walked out for miles, I'm not surprised
+     You said it was not about you and I
+     And I wish somehow, somehow you could stay
+     Can you stay for a while?
+     Stay for a while just the way we are
+     In the great divide
+     Inside, I will know when to let you go
+     Stay for a while just the way we are
+     You left without no goodbye in the night
+     Lost in a long-forgotten dream
+     You masquerade all you feel inside
+     Wherever you go somehow you find ways
+     To slowly keep on stealing
+     Concealing love that's left behind
+     And I wish somehow, somehow you could stay
+     Can you stay for a while?
+     Stay for a while just the way we are
+     Will I ever know when to let you go?
+     Just the way you are
+     I'll let you go just the way you are
+     
+     You said it was love
+     (Love was not enough for you and I)
+     You said it was love, oh
+     (Love was not enough for you and I)
+     You said it was love
+     (Love was not enough for you and I)
+     You said it was love
+     (Love was not enough)
+     (Enough)`
+        },
+        "EmptyNote":
+        {
+            url:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A293301390&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+            lyrics:`Empty Note:
+                
+     Ghostly Kisses
+     ---------------
+     How could I ever know you
+     When everything lies in disguise?
+
+     How could I ever forget
+     Those eyes, looking for my weal?
+
+     How could I ever know more
+     When everything is held by threat?
+
+     And how could I ever feel you
+     Once again, without losing my mind?
+
+     Ooh-ooh-ooh-ooh-ooh-ooh-ooh
+     Ooh-ooh-ooh-ooh-ooh-ooh-ooh-ooh-ooh
+     Ah-ah-ah-ah-ah-ah-ah
+     Ooh-ooh-ooh-ooh-ooh
+
+     Ooh, empty note
+     Shadows of my past
+     Made it to the end, hm
+     Ooh, empty note
+     Shadows of my past
+     Made it to the end
+
+     How could I ever know you
+     When you are miles and miles away?
+
+     How could I forgive myself?
+     How blind and scared I was?
+
+     Ooh, empty note
+     Shadows of my past
+     Made it to the end`
+        },
+        "Heartbeat":
+        {
+            url:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A1205544898&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+            lyrics:`Heartbeat:
+                    
+     Ghostly Kisses
+     ----------------
+     Come here
+     A little closer
+     Will you carry me away?
+     Stay forever and a day
+     Come here
+     A little closer
+     And I will never run away
+     No, I will never run away from you
+
+     Tell me I'm the one
+     Every time I fell in love, it went all wrong
+     You're my only one
+     Moon and sun
+     We've just begun to belong
+
+     Heartbeat, tell me what you need
+     Hold me, I know what you need now
+     Don't wanna turn it off
+     Don't wanna be alone
+
+     Heartbeat, tell mе what you need
+     Hold me, I know what you need now
+     Don't wanna turn it off
+     Don't wanna be alone
+
+     All night, lonеly
+     Every time when you're not here with me
+     All night, lonely
+     Every time when you're not here with me
+     All night, lonely
+     Every time when you're not here with me
+     All night, lonely
+     Every time when you're not here with me
+
+     Come here
+     And whisper in my ear
+     That you will never run away
+     No, you will never run away, promise
+     Tell me I'm the one
+     Every time I fell in love, it went all wrong
+     You're my only one
+     Moon and sun
+     We've just begun to belong
+
+     Heartbeat, tell me what you need
+     Hold me, I know what you need now
+     Don't wanna turn it off
+     Don't wanna be alone
+     Heartbeat, tell me what you need
+     Hold me, I know what you need now
+     Don't wanna turn it off
+     Don't wanna be alone
+
+     All night, lonely
+     Every time when you're not here with me
+     All night, lonely
+     Every time when you're not here with me
+     All night, lonely
+     Every time when you're not here with me
+     All night, lonely
+     Every time when you're not here with me`
+        }
+
+    },
+    Wisp:
+    {
+        "See You Soon":
+        {
+           // url:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A1726813965&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+           url:"https://audiomack.com//embed/jellaoe/song/see-you-soon",
+           lyrics:`See you soon:
+
+           
+
+         Wisp
+         ----
+         I'd trade hours for minutes with you
+         Never wanting something new
+         Pull me close, why'd you leave so soon?
+         Lay here with me, so I can gaze
+         Into your tender eyes
+         Missing you is missing all
+         House so big, my heart so small
+         To be with you is all I want
+         If you were to let me go
+         I'd nod off the world as whole
+         As my heart is with you
+         Our time together is overdue
+         I'd give all the stars to see you soon
+         Lay here with me so I can gaze
+         Into your tender eyes
+         Missing you is missing all
+         House so big, my heart so small
+         To fall so fast for someone who may never last`
+        },
+        "Your Face":
+        {
+           // url:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A1652122854&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+           url:"https://audiomack.com//embed/wisp/song/your-face", 
+           lyrics:`Your face:
+
+
+
+         Wisp
+         ----
+         Ah
+         Is this all real?
+         You're stuck in my head
+         Drowning
+         Falling into you
+         You look at me the same
+         But I can't reciprocate
+         Fragrant air
+         And then I see your face
+         I see your face
+         I see your face
+         I want to feel
+         Only when I'm with you
+         Black and white
+         I'm stuck in the loops of your mind
+         Black and white
+         I wish that I could call you mine
+         There is no
+         Going back
+         You look at me the same
+         But I can't reciprocate
+         Fragrant air
+         And then I see your face
+         I see your face
+         I see your face
+         Oh
+         Oh, oh`
+        },
+        "Once then we'll be free":
+        {
+            //url:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A1727965926&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+            url:"https://audiomack.com//embed/wisp/song/once-then-well-be-free",
+            lyrics:`Once then we'll be free:
+                
+
+
+         Wisp
+         ----
+         Live in time
+         Time when we
+         We had made
+         Made at sea
+         But now I stare
+         Stare into
+         Emptiness
+         Turned to miss
+         I'll find you in these waters
+         So reach your arm out to me
+         I promise it'll end soon
+         Once then we'll be free
+         Promise me
+         Me and you
+         You will be
+         Be here soon
+         But now I stare
+         Stare into
+         Emptiness
+         Turned to miss
+         I'll find you in these waters
+         So, reach your arm out to me
+         I promise it'll end soon
+         Once then we'll be free
+         Stay with me
+         In my arms
+         Stay with me
+         'Til we're free
+         Stay with me
+         In my arms
+         Stay with me
+         By my side`
+        },
+        "Pandora":
+        {
+           
+           // url:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A1787967565&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+            url:"https://audiomack.com//embed/wisp/song/pandora", 
+           lyrics:`Pandora:
+                    
+
+           
+         Wisp
+         ----
+         I'd bleed for you
+         If you held me close
+         My wounds open to your heart
+         That takes all my blood
+         Of you
+         Of you
+         I love for two
+         Haunting me, until my head
+         Numbs to all of what you said
+         And my wounds
+         Have opened
+         To all
+         Of you
+         Of you
+         I love for two
+         Dent my bones
+         My heart still lives
+         All of you
+         Please, hold me close
+         You once said
+         You love white roses
+         So, I'll grow flowers
+         Beneath my lungs
+         And my wounds
+         Have opened
+         To all
+         Of you
+         Of you
+         I love for two`
+        }
+    }
+}
